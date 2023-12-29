4e80ea83aae20959d76ab733e3820bc16021756c
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2023-12-29 17:11:48 +0900

    メインページを変更

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export const dynamic = 'force-dynamic'
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <h1>Hello, Vercel!</h1>
       <Link
         href="https://vercel.com/templates/next.js/postgres-prisma"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
